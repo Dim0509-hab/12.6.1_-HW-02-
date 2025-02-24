7f43a2a9f3e60ef1d46fb026f59c514548152b1d
Dim0509-hab <pripoy.kanifol@gmail.com>
2025-02-24 14:04:28 +0500

    Доработано

--- index.php
+++ index.php
@@ -8,11 +8,7 @@ $example_persons_array = [
         'fullname' => 'Степанова Наталья Степановна',
         'job' => 'frontend-developer',
     ],
-    [
-        'fullname' => 'Шакиров Димитрий Раисович',
-        'job' => 'frontend-developer',
-    ],
-    [
+        [
         'fullname' => 'Пащенко Владимир Александрович',
         'job' => 'analyst',
     ],
@@ -24,10 +20,7 @@ $example_persons_array = [
         'fullname' => 'Славин Семён Сергеевич',
         'job' => 'analyst',
     ],
-    [
-        'fullname' => 'Энзель Аида Маратовна',
-        'job' => 'frontend-developer',
-    ],
+    
     [
         'fullname' => 'Цой Владимир Антонович',
         'job' => 'frontend-developer',
@@ -54,234 +47,169 @@ $example_persons_array = [
     ],
 ];
 
+
+
 ///////////////////////////////////////////////////////
-function getFullnameFromParts($example_persons_array) {
-    $fullname1 = [];
 
-    foreach ($example_persons_array as $person) {
-        $fullname1[] = $person['fullname'];
-    }
-    /// Вывод в виде строк, содержащих сразу и фамилию, и имя, и отчество
-    //print_r($fullname1);    
-    return $fullname1;
-}
 
-$result1 = getFullnameFromParts($example_persons_array);
 
+function getFullnameFromParts($surname, $name, $patronymic) {
+    return $surname. ' '. $name. ' '. $patronymic;
+   }
 
 ////////////////////////////////////////////////////////
-function getPartsFromFullname($example_persons_array) {
-    $fullname = [];
 
-    foreach ($example_persons_array as $person) {
-        // Разбиваем строку на части
-        $parts = explode(' ', $person['fullname']);
 
-        $surname = $parts[0];
-        $name = $parts[1];
-        $patronymic = $parts[2];
 
-        // Добавляем элементы в новый массив
-        $fullname[] = [$name, $surname, $patronymic];        
-    }
-    /// Вывод в виде строк, содержащих по сточно имя, фамилию и отчество
-     //print_r($fullname);
-     
-    return $fullname;
-}
-$result2 = getFullnameFromParts($example_persons_array);
+function getPartsFromFullname($fullname) {
+    $parts = explode(' ', $fullname);
+    return [
+    'surname' => $parts[0],
+    'name' => $parts[1],
+    'patronymic' => $parts[2]
+    ];
+   }
+
+
+
 /////////////////////////////////////////////////////////////////
-function getShortName($parts) {
-    $name = $parts[0];
-    $surname = $parts[1];
 
-    // Форматируем строку вида «Иван И.»
-    $formattedName = $name . ' ' . substr($surname, 0, 2) . '.';
-    return $formattedName;
-}
 
-$shortNames = getPartsFromFullname($example_persons_array);
+function getShortName($fullname) {
+    // Разбиваем ФИО на части
+    $parts = getPartsFromFullname($fullname);
+   
+    // Извлекаем имя и фамилию
+    $name = $parts['name'];
+    $surname = $parts['surname'];
+   
+    // Возвращаем строку вида «Иван И.»
+    return $name. ' '. substr($surname, 0, 1). '.';
+   }
 
-// Выводим строку вида «Иван И.»
-foreach ($shortNames as $parts) {
-    //echo getShortName($parts) . PHP_EOL;
-    
-}
+
 ///////////////////////////////////////////////////////////
-$fullnames = getPartsFromFullname($example_persons_array);
 
-function getGenderFromName($fullnames) {
-    $surnamesEndV = [];
-    
-    foreach ($fullnames as $parts) {
-        $surname = $parts[1];
-        $name = $parts[0];
-        $patronymic= $parts[2];
-        $genderIndicator = 0;
-        
-        // Проверяем персоны на "...."
-        if (str_ends_with($name, 'н')) {
-            $genderIndicator++;
-        }
-        if (str_ends_with($name, 'й') ) {
-            $genderIndicator++;
-        }
-         if (str_ends_with($name, 'а')) {
-             $genderIndicator--;
-         }
-        if (str_ends_with($surname, 'в') ) {
-                $genderIndicator++;
-        } 
-        if (str_ends_with($surname, 'ва') ) {
-            $genderIndicator--;
-        }
-        if (str_ends_with($patronymic, 'ич') ) {
-                $genderIndicator++;
-        }        
-        if (str_ends_with($patronymic, 'вна') ) {
-            $genderIndicator--;
-        }        
-        if ($genderIndicator > 0 ) {
-            $formattedName = $surname . ' ' . substr($name, 0, 2) . '.' . substr($patronymic, 0, 2) .' '. '1' . ' ' . '(Мужской пол)';
-            $surnamesEndV[] = $formattedName;
-            
-        }
-        if ($genderIndicator < 0 ) {
-            $formattedName = $surname . ' ' . substr($name, 0, 2) . '.' . substr($patronymic, 0, 2) .' '. '-1' . ' ' . '(Женский пол)';
-            $surnamesEndV[] = $formattedName;
-        }
-        if ($genderIndicator == 0 ) {
-            $formattedName = $surname . ' ' . substr($name, 0, 2) . '.' . substr($patronymic, 0, 2) .' '. '0' . ' ' . '(неопределённый пол)';
-            $surnamesEndV[] = $formattedName;
-        }               
-            //print_r(($genderIndicator). PHP_EOL);       
+
+
+function getGenderFromName($fullname) {
+    // Разбиваем ФИО на части
+    $parts = getPartsFromFullname($fullname);
+   
+    // Извлекаем фамилию, имя и отчество
+    $surname = $parts['surname'];
+    $name = $parts['name'];
+    $patronymic = $parts['patronymic'];
+   
+    // Инициализируем суммарный признак пола
+    $genderIndicator = 0;
+   
+    // Проверяем отчество
+    if (str_ends_with($patronymic, 'ич')) {
+    $genderIndicator++;
+    } elseif (str_ends_with($patronymic, 'вна')) {
+    $genderIndicator--;
     }
-    return $surnamesEndV;
-}
-
-$surnamesEndV = getGenderFromName($fullnames);
-
-// Выводим определения пола по ФИО
-foreach ($surnamesEndV as $surname) {
-   //echo $surname . PHP_EOL;
-}
+   
+    // Проверяем имя
+    if (str_ends_with($name, 'й') || str_ends_with($name, 'н')) {
+    $genderIndicator++;
+    } elseif (str_ends_with($name, 'а')) {
+    $genderIndicator--;
+    }
+   
+    // Проверяем фамилию
+    if (str_ends_with($surname, 'в')) {
+    $genderIndicator++;
+    } elseif (str_ends_with($surname, 'ва')) {
+    $genderIndicator--;
+    }
+   
+    // Возвращаем результат в зависимости от суммарного признака пола
+    if ($genderIndicator > 0) {
+    return 1; // Мужской пол
+    } elseif ($genderIndicator < 0) {
+    return -1; // Женский пол
+    } else {
+    return 0; // Неопределённый пол
+    }
+   }
 
 //////////////////////////////////////////////////////////////
 
-function getGenderDescription($fullnames) {
-    $genderData = getGenderFromName($fullnames);
+
+function getGenderDescription($persons) {
+
     $maleCount = 0;
     $femaleCount = 0;
     $unknownCount = 0;
-    $totalCount = count($genderData);
-
-    foreach ($genderData as $entry) {
-        if (strpos($entry, '(Мужской пол)') !== false) {
-            $maleCount++;
-        } elseif (strpos($entry, '(Женский пол)') !== false) {
-            $femaleCount++;
-        } elseif (strpos($entry, '(неопределённый пол)') !== false) {
-            $unknownCount++;
-        }
+   
+    
+    foreach ($persons as $fullname) {
+    $gender = getGenderFromName($fullname);
+   
+    if ($gender === 1) {
+    $maleCount++;
+    } elseif ($gender === -1) {
+    $femaleCount++;
+    } else {
+    $unknownCount++;
     }
+    }
+   
+    // Общее количество персон
+    $totalCount = count($persons);
+   
+    
+    $malePercent = ($totalCount > 0) ? ($maleCount / $totalCount) * 100 : 0;
+    $femalePercent = ($totalCount > 0) ? ($femaleCount / $totalCount) * 100 : 0;
+    $unknownPercent = ($totalCount > 0) ? ($unknownCount / $totalCount) * 100 : 0;
+   
+    // Округляем проценты до одного знака после запятой
+    $malePercent = round($malePercent, 1);
+    $femalePercent = round($femalePercent, 1);
+    $unknownPercent = round($unknownPercent, 1);
+   
+    
+    $result = "Гендерный состав аудитории:\n";
+    $result .= "---------------------------\n";
+    $result .= "Мужчины - ". $malePercent. "%\n";
+    $result .= "Женщины - ". $femalePercent. "%\n";
+    $result .= "Не удалось определить - ". $unknownPercent. "%\n";
+   
+    return $result;
+   }
 
-    $malePercentage = ($maleCount / $totalCount) * 100;
-    $femalePercentage = ($femaleCount / $totalCount) * 100;
-    $unknownPercentage = ($unknownCount / $totalCount) * 100;
-
-    return [
-        'Мужчины' => sprintf('%.1f', $malePercentage) . '%',
-        'Женщины' => sprintf('%.1f', $femalePercentage) . '%',
-        'Не удалось определить' => sprintf('%.1f', $unknownPercentage) . '%'
-    ];
-}
-
-$genderDescription = getGenderDescription($fullnames);
-
-
-// Выводим определение возрастно-полового состава
-echo "Гендерный состав аудитории:\n";
-echo "---------------------------\n";
-echo "Мужчины - " . $genderDescription['Мужчины'] . "\n";
-echo "Женщины - " . $genderDescription['Женщины'] . "\n";
-echo "Не удалось определить - " . $genderDescription['Не удалось определить'] . "\n";
 
 ////////////////////////////////////////////////////
 
-function getPerfectPartner($fullnames) {
-    // Функция для определения пола по ФИО    
-    function getGenderFromName1($fullname) {
-        $name = $fullname[0];
-        $surname = $fullname[1];
-        $patronymic = $fullname[2];
-        $genderIndicator = 0;
-
-        // Проверяем имя
-        if (str_ends_with($name, 'н')) {
-            $genderIndicator++;
-        }
-        if (str_ends_with($name, 'й') ) {
-            $genderIndicator++;
-        }
-        if (str_ends_with($name, 'а')) {
-            $genderIndicator--;
-        }
-
-        // Проверяем фамилию
-        if (str_ends_with($surname, 'в')) {
-            $genderIndicator++;
-        }
-        if (str_ends_with($surname, 'ва')) {
-            $genderIndicator--;
-        }
-
-        // Проверяем отчество
-        if (str_ends_with($patronymic, 'ич')) {
-            $genderIndicator++;
-        }
-        if (str_ends_with($patronymic, 'вна')) {
-            $genderIndicator--;
-        }
-
-        if ($genderIndicator > 0) {
-            return 'Мужской пол';
-        } elseif ($genderIndicator < 0) {
-            return 'Женский пол';
-        } else {
-            return 'Неопределённый пол';
-        }
-    }
-
-    // Выбираем случайные персоны
+function getPerfectPartner($surname, $name, $patronymic, $persons) {
+    // Приводим ФИО к привычному регистру
+    $surname = ucwords(strtolower($surname));
+    $name = ucwords(strtolower($name));
+    $patronymic = ucwords(strtolower($patronymic));
+   
+    
+    $fullname = getFullnameFromParts($surname, $name, $patronymic);
+   
+    // Определяем пол для ФИО
+    $gender = getGenderFromName($fullname);
+   
+    // Выбираем случайного человека из массива
     do {
-        $randomIndex1 = array_rand($fullnames);
-        $randomIndex2 = $randomIndex1;
-        while ($randomIndex2 === $randomIndex1) {
-            $randomIndex2 = array_rand($fullnames);
-        }       
-        $person1 = $fullnames[$randomIndex1];
-        $person2 = $fullnames[$randomIndex2];       
-        // Определяем пол выбранных персон
-        $gender1 = getGenderFromName1($person1);
-        $gender2 = getGenderFromName1($person2);
-       
-    } while (($gender1 === $gender2) || ($gender1 === 'Неопределённый пол') || ($gender2 === 'Неопределённый пол'));
-
-    // Проверяем, что персоны противоположного пола
-    if (($gender1 === 'Мужской пол' && $gender2 === 'Женский пол') || ($gender1 === 'Женский пол' && $gender2 === 'Мужской пол')) {
-        // Форматируем ФИО
-        $formattedName1 = $person1[1] . ' ' . substr($person1[0], 0, 2) . '.';
-        $formattedName2 = $person2[1] . ' ' . substr($person2[0], 0, 2) . '.';
-
-        // Генерируем случайный процент совместимости
-        $compatibility = mt_rand(5000, 10000) / 100;
-
-        // Формируем результат
-        $result = $formattedName1 . ' + ' . $formattedName2 . ' = ' . "\n♡ Идеально на " . sprintf('%.2f', $compatibility) . "% ♡";
-        return $result;
-    } else {
-        return "Не удалось найти пару противоположного пола.";
-    }
-}
-
-echo getPerfectPartner($fullnames);
+    $randomIndex = array_rand($persons);
+    $randomPerson = $persons[$randomIndex];
+    $randomGender = getGenderFromName($randomPerson);
+    } while ($gender === $randomGender); // Повторяем выбор, если пол совпадает
+   
+    // Склеиваем ФИО случайного человека
+    list($randomSurname, $randomName, $randomPatronymic) = explode(' ', $randomPerson);
+    $randomShortName = getShortName($randomPerson);
+   
+    // Форматируем результат
+    $compatibility = mt_rand(5000, 10000) / 100; // Случайное число от 50 до 100 с двумя знаками после запятой
+    $result = ucfirst($name). ' '. substr($surname, 0, 1). '. + '. $randomShortName. ' = \n';
+    $result.= '♡ Идеально на '. number_format($compatibility, 2). '% ♡';
+   
+    return $result;
+   }
